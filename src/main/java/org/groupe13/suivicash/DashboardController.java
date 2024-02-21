4cc2965a9940c7e95585d247a7f7f23df6789344
WallyDev <williams25prince@gmail.com>
2024-02-21 21:51:23 +0100

    update

--- src/main/java/org/groupe13/suivicash/DashboardController.java
+++ src/main/java/org/groupe13/suivicash/DashboardController.java
@@ -10,12 +10,12 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.ListView;
 import javafx.scene.control.Label;
-import javafx.stage.Modality;
-import org.groupe13.suivicash.modele.*;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import org.groupe13.suivicash.modele.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -25,24 +25,10 @@ import java.sql.SQLException;
 public class DashboardController {
 
     @FXML
-    private ListView<Banque> banqueListView;
+    private TableView<Banque> banqueTableView;
 
     @FXML
     private Label totalSoldeLabel;
-
-    @FXML
-    private Label totalDepensesLabel;
-
-    @FXML
-    private Label totalRevenusLabel;
-
-    @FXML
-    private BarChart<String, Number> depensesBarChart;
-
-    @FXML
-    private BarChart<String, Number> revenusBarChart;
-
-    private Stage stage;
 
     private ObservableList<Banque> banques;
 
@@ -53,121 +39,39 @@ public class DashboardController {
         connection = connectionFile.getConnection();
         loadBanquesFromDatabase();
         calculerTotalSolde();
-        /*calculerTotalDepenses();
-        calculerTotalRevenus();
-        afficherDepensesParMois();
-        afficherRevenusParMois();*/
     }
 
     @FXML
     private void handleAjouterBanqueClick() {
         try {
-            // Charger le fichier FXML de la nouvelle fenêtre
             FXMLLoader loader = new FXMLLoader(getClass().getResource("vues/AjoutBanque.fxml"));
             Parent root = loader.load();
 
-            // Créer une nouvelle scène
             Scene scene = new Scene(root);
 
-            // Créer une nouvelle fenêtre modale
             Stage stage = new Stage();
             stage.setTitle("Ajouter Banque");
             stage.setScene(scene);
             stage.initModality(Modality.APPLICATION_MODAL);
 
-            // Afficher la fenêtre
             stage.showAndWait();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void setStage(Stage stage) {
+    /*public void setStage(Stage stage) {
         this.stage = stage;
-    }
+    }*/
 
     private void loadBanquesFromDatabase() {
         banques = FXCollections.observableArrayList();
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT NomBanque, Solde FROM banques");
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                String nomBanque = resultSet.getString("NomBanque");
-                double solde = resultSet.getDouble("Solde");
-                Banque banque = new Banque(nomBanque, solde);
-                banques.add(banque);
-            }
-            banqueListView.setItems(banques);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        banques.addAll(Banque.getAllBanques(connection));
+        banqueTableView.setItems(banques);
     }
 
     private void calculerTotalSolde() {
-        double totalSolde = 0;
-        for (Banque banque : banques) {
-            totalSolde += banque.getSolde();
-        }
+        double totalSolde = Banque.getTotalSolde(connection);
         totalSoldeLabel.setText(String.format("%.2f", totalSolde));
-    }
-
-    private void calculerTotalDepenses() {
-        double totalDepenses = 0;
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT SUM(Montant) AS TotalDepenses FROM depenses");
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                totalDepenses = resultSet.getDouble("TotalDepenses");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        totalDepensesLabel.setText(String.format("%.2f", totalDepenses));
-    }
-
-    private void calculerTotalRevenus() {
-        double totalRevenus = 0;
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT SUM(Montant) AS TotalRevenus FROM revenus");
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                totalRevenus = resultSet.getDouble("TotalRevenus");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        totalRevenusLabel.setText(String.format("%.2f", totalRevenus));
-    }
-
-    private void afficherDepensesParMois() {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT Mois, SUM(Montant) AS TotalDepenses FROM depenses GROUP BY Mois");
-            ResultSet resultSet = statement.executeQuery();
-            XYChart.Series<String, Number> series = new XYChart.Series<>();
-            while (resultSet.next()) {
-                String mois = resultSet.getString("Mois");
-                double totalDepenses = resultSet.getDouble("TotalDepenses");
-                series.getData().add(new XYChart.Data<>(mois, totalDepenses));
-            }
-            depensesBarChart.getData().add(series);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void afficherRevenusParMois() {
-        try {
-            PreparedStatement statement = connection.prepareStatement("SELECT Mois, SUM(Montant) AS TotalRevenus FROM revenus GROUP BY Mois");
-            ResultSet resultSet = statement.executeQuery();
-            XYChart.Series<String, Number> series = new XYChart.Series<>();
-            while (resultSet.next()) {
-                String mois = resultSet.getString("Mois");
-                double totalRevenus = resultSet.getDouble("TotalRevenus");
-                series.getData().add(new XYChart.Data<>(mois, totalRevenus));
-            }
-            revenusBarChart.getData().add(series);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/org/groupe13/suivicash/modele/Banque.java
+++ src/main/java/org/groupe13/suivicash/modele/Banque.java
@@ -3,13 +3,20 @@ package org.groupe13.suivicash.modele;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 public class Banque {
+    private int IDBanque;
     private String nomBanque;
     private double solde;
 
-    public Banque(String nomBanque, double solde) {
+    public Banque(int IDBanque, String nomBanque, double solde) {
+        this.IDBanque = IDBanque;
         this.nomBanque = nomBanque;
         this.solde = solde;
+    }
+
+    public int getIDBanque() {
+        return IDBanque;
     }
 
     public String getNomBanque() {
@@ -23,5 +30,39 @@ public class Banque {
     @Override
     public String toString() {
         return nomBanque;
+    }
+
+    // Méthode pour récupérer la liste des banques depuis la base de données
+    public static List<Banque> getAllBanques(Connection connection) {
+        List<Banque> banques = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM banques");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int IDBanque = resultSet.getInt("IDBanque");
+                String nomBanque = resultSet.getString("NomBanque");
+                double solde = resultSet.getDouble("Solde");
+                Banque banque = new Banque(IDBanque, nomBanque, solde);
+                banques.add(banque);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return banques;
+    }
+
+    // Méthode pour récupérer le solde total de toutes les banques depuis la base de données
+    public static double getTotalSolde(Connection connection) {
+        double totalSolde = 0;
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT SUM(Solde) AS TotalSolde FROM banques");
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                totalSolde = resultSet.getDouble("TotalSolde");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return totalSolde;
     }
 }
