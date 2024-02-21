ffaf98d4f053f25060d770adefff6ee4774c1f93
WallyDev <williams25prince@gmail.com>
2024-02-21 11:02:56 +0100

    update

--- src/main/java/org/groupe13/suivicash/SuiviCash.java
+++ src/main/java/org/groupe13/suivicash/SuiviCash.java
@@ -11,7 +11,7 @@ public class SuiviCash extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SuiviCash.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 610);
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 610);
         stage.setTitle("SuiviCash💰");
         stage.setScene(scene);
         stage.show();
