1ed922891cc80b2b627f22625f4149d8370084cd
dbmj47 <marcdaha47@gmail.com>
2024-02-22 15:24:17 +0100

    modif_interface & reglage pb connection bd

--- src/main/java/org/groupe13/suivicash/modele/connectionFile.java
+++ src/main/java/org/groupe13/suivicash/modele/connectionFile.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class connectionFile {
     // Les informations de connexion à la base de données MySQL
-    private static final String URL = "jdbc:mysql://localhost:3306/suivicash";
+    private static final String URL = "jdbc:mysql://localhost:3306/suivicash?useSSL=false&serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
 
